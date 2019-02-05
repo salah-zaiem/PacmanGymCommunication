d96a18a60de39c60de649b80043db4d4cb039705
ebennequin <etienne.bennequin@gmail.com>
2019-02-05 11:10:59 +0100

    testetienne => test

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup
-print("testetienne")
+print("test")
 setup(name='gym_pacman',
       version='0.0.1',
       install_requires=['gym']  # And any other dependencies foo needs
